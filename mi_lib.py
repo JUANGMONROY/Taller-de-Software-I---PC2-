4cd25a04d426b64481dd1361b3960ca7b8337990
JUANGMONROY <66014655+JUANGMONROY@users.noreply.github.com>
2020-06-24 08:02:12 -0500

    Agrego la primera función
    agrego función del ejercicio 2 de la practica

--- mi_lib.py
+++ mi_lib.py
@@ -1,1 +1,12 @@
+#1 funcion con libreria
+from random import randint
 
+def lista2D(m,n):
+    lista=[]
+
+    for i in range(m):
+            lista.append([0]*n) #HAGO LA LISTA DE LISTAS
+    for i in range(m):
+        for j in range(n):
+            lista[i][j]= randint(0,100) #LLENO LA LISTA CON VALORES RANDOM
+    return lista #retorno la lista
