cc5ce1c656d6de38d257139781d67761bbbcee09
JUANGMONROY <66014655+JUANGMONROY@users.noreply.github.com>
2020-06-24 08:08:35 -0500

    Agrego ultimo ejercicio a principal
    Agrego mi ultimo ejercicio a mi archivo principal

--- principal.py
+++ principal.py
@@ -1,1 +1,19 @@
+#Ejercicio 4 con su libreria
+import numpy as np
+def crea_arreglo():
+    n = np.random.randint(5,21,size=(20,20)) #Creo mi matriz con valores aleatorios
+    print("============================Original============================")
+    print(n) #Imprimo la matriz original
+    print("================================================================")
+    
+    for i in range(20):
+        print((i+1),"ITERACIÓN") #Voy enumerando las iteraciones
+        x = n[:,0] #Guardo la primera columna que se va a pasear por el arreglo
+        x = np.reshape(x,(20,1)) #Reformo la variable x para que sea solo una columna
 
+        h=n[:,1:20] #Elimino las primera columna para que las demas avanzen 
+        n = np.append(h,x,axis=1) #Añado la columna que guarde en un principio como columna final del arreglo
+
+        print(n) #Va imprimir 20 veces y nuestra columna 0 se va a pasear por todo el arreglo hasta ser como el arreglo original
+        print("================================================================")
+    return "Finalizado"
